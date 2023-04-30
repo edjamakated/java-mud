c0ca90709a93a5226871e3cb7e89630d26e6f0c2
Webhosting That's Fun <rdecker@gmail.com>
2023-04-30 08:51:49 -0700

    Update game.java

--- game.java
+++ game.java
@@ -1,69 +1,220 @@
-// Character.java
-public class Character {
-    String name;
-    int x;
-    int y;
-    int health;
-    int level;
-    List<Item> inventory;
-    String id;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-    public void move(String direction) {}
-    public void attack(Character target) {}
-    public void pickup(Item item) {}
-    public void use(Item item) {}
-    public void showInventory() {}
-    public void saveToDB() {}
-    public void loadFromDB() {}
+// Existing classes from your previous code...
+
+// New classes
+
+public class CollisionDetector {
+    public void detectCollisions(Map map) {}
 }
 
-// NPC.java
-public class NPC {
-    String name;
-    int x;
-    int y;
-    String dialogue;
-    String id;
-
-    public void interact() {}
-    public void move(String direction) {}
-    public void saveToDB() {}
-    public void loadFromDB() {}
+public class GameEvent {
+    // Implement event properties and methods as needed
 }
 
-// Item.java
-public class Item {
-    String name;
-    String description;
-    String effect;
-    String id;
+public class GameState {
+    private boolean gameOver;
+    private boolean gameWon;
 
-    public void use() {}
-    public void saveToDB() {}
-    public void loadFromDB() {}
+    public void checkWinCondition() {}
+    public void checkLossCondition() {}
 }
 
-// Map.java
-public class Map {
-    int size;
-    List<Character> characters;
-    List<NPC> npcs;
-    List<Item> items;
-    String id;
-
-    public void addCharacter(Character character) {}
-    public void addNPC(NPC npc) {}
-    public void addItem(Item item) {}
-    public void showMap() {}
-    public void saveToDB() {}
-    public void loadFromDB() {}
+public class Chat {
+    public void sendMessage(String message) {}
+    public void receiveMessage(String message) {}
 }
 
-// Game.java
+// Updated Client class with networking code using java.net
+
+public class Client {
+    Socket socket;
+    Character character;
+    BufferedReader in;
+    PrintWriter out;
+
+    public Client(String serverAddress, int port) throws IOException {
+        socket = new Socket(serverAddress, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
+    public String receiveMessage() throws IOException {
+        return in.readLine();
+    }
+
+    public void updateCharacter() {}
+    public void displayMap() {}
+}
+
+// Updated Server class with networking code using java.net
+
+public class Server {
+    ServerSocket serverSocket;
+    List<ClientHandler> clients;
+    Map map;
+    Database database;
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clients = new CopyOnWriteArrayList<>();
+    }
+
+    public void listenForConnections() throws IOException {
+        while (true) {
+            Socket socket = serverSocket.accept();
+            ClientHandler clientHandler = new ClientHandler(socket);
+            clients.add(clientHandler);
+            clientHandler.start();
+        }
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    public void updateMap() {}
+    public void updateDatabase() {}
+
+    private class ClientHandler extends Thread {
+        Socket socket;
+        BufferedReader in;
+        PrintWriter out;
+
+        public ClientHandler(Socket socket) throws IOException {
+            this.socket = socket;
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+
+        public String receiveMessage() throws IOException {
+            return in.readLine();
+        }
+
+        @Override
+        public void run() {
+            // Handle incoming messages and other tasks here
+        }
+    }
+}
+// Updated Database class with basic JDBC functionality and MySQL
+
+public class Database {
+    Connection connection;
+
+    public Database(String url, String user, String password) throws SQLException {
+        connection = DriverManager.getConnection(url, user, password);
+    }
+
+    public void executeQuery(String query) throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.execute(query);
+    }
+
+    public ResultSet getData(String table, String conditions) throws SQLException {
+        Statement stmt = connection.createStatement();
+        String query = "SELECT * FROM " + table + " WHERE " + conditions;
+        return stmt.executeQuery(query);
+    }
+
+    public void insertData(String table, Map<String, String> data) throws SQLException {
+        StringBuilder columns = new StringBuilder();
+        StringBuilder values = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            columns.append(entry.getKey()).append(", ");
+            values.append("'").append(entry.getValue()).append("', ");
+        }
+
+        columns.setLength(columns.length() - 2); // Remove trailing comma and space
+        values.setLength(values.length() - 2);
+
+        String query = "INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ")";
+        executeQuery(query);
+    }
+
+    public void updateData(String table, Map<String, String> data, String conditions) throws SQLException {
+        StringBuilder updates = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            updates.append(entry.getKey()).append(" = '").append(entry.getValue()).append("', ");
+        }
+
+        updates.setLength(updates.length() - 2); // Remove trailing comma and space
+
+        String query = "UPDATE " + table + " SET " + updates + " WHERE " + conditions;
+        executeQuery(query);
+    }
+}
+
+// Simple graphics using Swing
+
+public class GameWindow extends JFrame {
+    private JPanel mainPanel;
+
+    public GameWindow() {
+        setTitle("2D Multiplayer Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        mainPanel = new JPanel();
+        mainPanel.setBackground(Color.WHITE);
+        add(mainPanel);
+        setVisible(true);
+    }
+
+    public void drawMap(Map map) {
+        // Implement drawing logic based on the map object
+    }
+}
+
 public class Game {
     Character player;
     Map map;
     Database database;
+    GameWindow gameWindow;
+
+    public Game() throws SQLException {
+        // Initialize the game window
+        gameWindow = new GameWindow();
+
+        // Initialize the database connection
+        String url = "jdbc:mysql://localhost:3306/your_database_name";
+        String user = "your_database_user";
+        String password = "your_database_password";
+        database = new Database(url, user, password);
+
+        // Load map, player, and other data from the database
+        loadFromDB();
+    }
 
     public void start() {}
     public void play() {}
@@ -72,36 +223,13 @@ public class Game {
     public void loadFromDB() {}
 }
 
-// Client.java
-public class Client {
-    Socket socket;
-    Character character;
-
-    public void sendMessage(String message) {}
-    public void receiveMessage() {}
-    public void updateCharacter() {}
-    public void displayMap() {}
-}
-
-// Server.java
-public class Server {
-    Socket socket;
-    List<Client> clients;
-    Map map;
-    Database database;
-
-    public void listenForConnections() {}
-    public void broadcastMessage(String message) {}
-    public void updateMap() {}
-    public void updateDatabase() {}
-}
-
-// Database.java
-public class Database {
-    Connection connection;
-
-    public void executeQuery(String query) {}
-    public ResultSet getData(String table, String conditions) {}
-    public void insertData(String table, Map<String, String> data) {}
-    public void updateData(String table, Map<String, String> data, String conditions) {}
+public static void main(String[] args) {
+    EventQueue.invokeLater(() -> {
+        try {
+            Game game = new Game();
+            game.start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    });
 }
